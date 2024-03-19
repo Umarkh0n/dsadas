d4946cd6c9686af0febb7ed7261a08135e1058e8
Umarkhon <kudratkhujaev@bk.ru>
2024-03-19 19:27:54 +0500

    init

---
+++ js/index.js
@@ -0,0 +1,10 @@
+let firstName = 'Umarkhon.'
+
+let secondName = 'Kudratkhujaev.'
+
+let age = 23
+
+let job = 'darturchi.'
+
+
+console.log('Mening ismim: ' + firstName + ' Mening familiyam: ' + secondName + ' Mening yoshim: ' + age + ' Mening ishim: ' + job );
